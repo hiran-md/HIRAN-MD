5fdeda3654bfa956200c2b125508432157ab2a23
hiran-md <hiranmdbot@gmail.com>
2024-11-01 19:06:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "9780Tbbb#16Z7ZtKmAUBWlaWNiPkPctqZJBneOxgUQPXNhZWistM",
+ALIVE_IMG: prrocess.env.ALIVE_IMG || "https://avatars.githubusercontent.com/u/187024208?s=96&v=4",,
+ALIVE_MSG: .process.env.ALIVE_MSG || "Hello i am alive now",
 };
