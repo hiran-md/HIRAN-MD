74380b1959dda06ee84a878ac3a2a61fb094d242
hiran-md <hiranmdbot@gmail.com>
2024-11-01 19:30:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "9780Tbbb#16Z7ZtKmAUBWlaWNiPkPctqZJBneOxgUQPXNhZWistM",
+SESSION_ID: process.env.SESSION_ID || "qctgkCLT#hDM_0mJgGAeqYVjxuf_V3FJIkmtxSXGXQdfufa-E4Q4",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://avatars.githubusercontent.com/u/187024208?s=96&v=4",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello i am alive now",
 };
