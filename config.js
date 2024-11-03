213252bae5139c4fbb3e4784b3fc7d2d63ef507b
hiran-md <hiranmdbot@gmail.com>
2024-11-03 17:00:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "029RhRgR#5CwH9FCGlgqne1D1-31bkK8fM3vwTOV26NnyiwoxgdM",
-MONGODB: process.env.MONGODB || "mongodb://mongo:KqJnmqHCeXKVyXNTSJkBxlsoxfjyLBwD@mongodb.railway.internal:27017"
+MONGODB: process.env.MONGODB || "mongodb://mongo:kedItnIxCFmFrrOwCwtmucCdJYNwPEuf@autorack.proxy.rlwy.net:49768"
 };
