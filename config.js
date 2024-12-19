58907391aba32589c600a656f5b433811d8156be
Hiranya Sathsara <hiranmdbot@gmail.com>
2024-12-19 13:13:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "029RhRgR#5CwH9FCGlgqne1D1-31bkK8fM3vwTOV26NnyiwoxgdM",
+SESSION_ID: process.env.SESSION_ID || "VJgx1B7Y#ofODRyiUl9lnMYrAOjZ14eaBTkRl7htjbf7dvou6hm0",
 MONGODB: process.env.MONGODB || "mongodb://mongo:kedItnIxCFmFrrOwCwtmucCdJYNwPEuf@autorack.proxy.rlwy.net:49768"
 };
